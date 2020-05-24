e243d6975dbe8268560dbc86e0603d04c377bd8c
hacers <hacer.karademir@gmail.com>
2020-05-24 19:31:46 +0100

    update BasePage

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -1,4 +1,46 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.BrowserUtils;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    /**
+     * This method will navigate user to the specific module in vytrack application.
+     * For example: if tab is equals to Activities, and module equals to Calls,
+     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
+     *
+     * @param tab
+     * @param module
+     */
+    public void navigateToModule(String tab, String module) {
+        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
+        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
+        try {
+            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
+            WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
+            new Actions(Driver.get()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
+        } catch (Exception e) {
+            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
+        }
+        try {
+            BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
+            BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
+            BrowserUtils.scrollToElement(Driver.get().findElement(By.xpath(moduleLocator)));
+            Driver.get().findElement(By.xpath(moduleLocator)).click();
+        } catch (Exception e) {
+//            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
+            BrowserUtils.clickWithTimeOut(Driver.get().findElement(By.xpath(moduleLocator)),  5);
+        }
+    }
+
 }
